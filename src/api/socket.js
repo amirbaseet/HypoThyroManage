35019990e7fcc2a1ae37ed42ed8b1df5524da624
amirbaseet <amro.baseet@ogr.sakarya.edu.tr>
2025-03-30 01:57:28 +0300

    adding https

--- src/api/socket.js
+++ src/api/socket.js
@@ -19,7 +19,7 @@ const connectSocket = async () => {
             return null;
         }
 
-        socket = io(SOCKET_URL, {
+        socket = io("http://185.149.103.143:3001", {
             transports: ["websocket", "polling"],
             path: "/socket.io/",
             reconnection: true,
